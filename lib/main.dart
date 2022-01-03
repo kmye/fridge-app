4487ab2544c5f1bd89fc5af833faa4d7baf36db7
Eileen Kang <eileen_kang@tech.gov.sg>
2022-01-03 13:57:13 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class MyFridgeApp extends StatelessWidget {
+  const MyFridgeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
