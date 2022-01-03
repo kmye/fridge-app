d2f72a1fb251f03f14d74d0514f62b5dd4ad03c1
Eileen Kang <eileen_kang@tech.gov.sg>
2022-01-03 14:07:09 +0800

    Add hello world codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
+ runApp(MyFridgeApp());
 }
 
 class MyFridgeApp extends StatelessWidget {
-  const MyFridgeApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return const MaterialApp(home: Text("Hello"));
   }
 }
